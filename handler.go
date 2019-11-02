3b0581e5be0a7b1e590a8ba4bcfd61f70f54ac37
stan wielga <svwielga4@gmail.com>
2019-11-02 10:57:29 -0700

    everything is working with map redirect

--- handler.go
+++ handler.go
@@ -12,7 +12,14 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	//	TODO: Implement this...
-	return nil
+	return func(rw http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(rw, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(rw, r)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
